c1c72b4348503e325e6e2b7405711d4d58b828e0
Gabbie Heinz <gabbieheinz@Gabbies-MacBook-Air.local>
2025-10-25 09:30:45 -0500

    Add failing test for home page message

---
+++ status_monitor/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from django.urls import reverse
+class HomeViewTest(TestCase):
+    def test_home_page_loads_successfully(self):
+        response=self.client.get(reverse('home'))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response, "Welcome to the status monitor")
